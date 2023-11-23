902af1bc63cc951a0712159650b2bdea8c65f9fc
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-11-23 08:28:09 +0000

    Adding eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+        '*.svelte'
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+        'parser': '@typescript-eslint/parser'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+}
