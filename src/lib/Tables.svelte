f620e3aaea0c37278f6685e894e00fbe58bf7b65
Benoit-Welsch <56845767+Benoit-Welsch@users.noreply.github.com>
2024-06-19 09:35:16 +0000

    Feat : Sort question manually (drag and drop)

--- src/lib/Tables.svelte
+++ src/lib/Tables.svelte
@@ -16,6 +16,43 @@
   const changeSelection = () => {
     questions.update((o) => o.map((q) => ({ ...q, show: checked })));
   };
+
+  let mouseYCoordinate = null; // pointer y coordinate within client
+  let distanceTopGrabbedVsPointer = null;
+
+  let draggingItem = null;
+  let draggingItemId = null;
+  let draggingItemIndex = null;
+
+  let hoveredItemIndex = null;
+
+  $: {
+    // prevents the ghost flickering at the top
+    if (mouseYCoordinate == null || mouseYCoordinate == 0) {
+      // showGhost = false;
+    }
+  }
+
+  $: {
+    if (
+      draggingItemIndex != null &&
+      hoveredItemIndex != null &&
+      draggingItemIndex != hoveredItemIndex
+    ) {
+      // swap items
+      questions.update((list) => {
+        [list[draggingItemIndex], list[hoveredItemIndex]] = [
+          list[hoveredItemIndex],
+          list[draggingItemIndex],
+        ];
+        return list;
+      });
+      // balance
+      draggingItemIndex = hoveredItemIndex;
+    }
+  }
+
+  let container = null;
 </script>
 
 {#if !$questions || $questions.length}
@@ -29,7 +66,16 @@
         bind:value={text}
       />
     </div>
-    <ul>
+    <ul bind:this={container}>
+      {#if mouseYCoordinate}
+        <li
+          class="item ghost"
+          style="top: {mouseYCoordinate +
+            distanceTopGrabbedVsPointer}px; background: {draggingItem.value};"
+        >
+          {draggingItem.value}
+        </li>
+      {/if}
       <li>
         <input
           type="checkbox"
@@ -40,11 +86,37 @@
         <label for="show-all-q">All</label>
       </li>
       {#each $questions as question, i}
-        <li>
+        <li on:dragover|preventDefault>
           <input
             type="checkbox"
             id="{question.title}-{i}"
             bind:checked={question.show}
+          />
+          <img
+            src="/reorder-three.svg"
+            alt=""
+            draggable="true"
+            on:dragstart={(e) => {
+              mouseYCoordinate = e.clientY;
+              //console.log('dragstart', mouseYCoordinate);
+
+              draggingItem = question;
+              draggingItemIndex = i;
+              draggingItemId = i;
+
+              distanceTopGrabbedVsPointer =
+                e.target.getBoundingClientRect().y - e.clientY;
+            }}
+            on:drag={(e) => {
+              mouseYCoordinate = e.clientY;
+            }}
+            on:dragover={(e) => {
+              hoveredItemIndex = i;
+            }}
+            on:dragend={(e) => {
+              draggingItemId = null; // makes item visible
+              hoveredItemIndex = null; // prevents instant swap
+            }}
           />
           <label for="{question.title}-{i}">{question.title}</label>
         </li>
@@ -66,5 +138,26 @@
     padding-left: 2px;
     overflow-y: scroll;
     max-height: 40vh;
+    position: relative;
+  }
+  li {
+    display: flex;
+    gap: 0.2rem;
+    align-items: center;
+  }
+  li img {
+    margin-bottom: -0.2rem;
+    width: 1.2rem;
+    height: 1.2rem;
+    cursor: grab;
+  }
+  .ghost {
+    margin-bottom: 10px;
+    pointer-events: none;
+    z-index: 99;
+    position: absolute;
+    top: 0;
+    left: 10;
+    background-color: white;
   }
 </style>
